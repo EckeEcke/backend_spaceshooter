de41d006feed3ff98186615234b60b128ff3a38d
Christian Eckardt <eckardt.christian@web.de>
2021-05-02 18:40:30 +0200

    initial commit

---
+++ spaceshooter-backend.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const mysql = require('mysql');
+const con = mysql.createConnection({
+  host: "ulsq0qqx999wqz84.chr7pe7iynqr.eu-west-1.rds.amazonaws.com",
+  database: "qzl4o8akbn6y17dv",
+  user: "mpwyg8k3gepwa8ib",
+  password: "cgyqdt5o7p6l3q6w",
+  port: 3306
+});
+
+let sql = `SELECT * FROM highscores ORDER BY Score DESC`;
+
+con.connect(function(err) {
+  if(err) throw err;
+  console.log("Connected!");
+  con.query(sql, function (err, result) {
+     if (err) throw err;
+     console.log("Result: " + result);
+  });
+});
+
+app.get('/', function (req, res) {
+  con.query(sql, function(err,result){
+    if (err) throw err;
+    else {
+      res.send(result);
+    }})
+}).listen(3000);

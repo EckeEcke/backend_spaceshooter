3a1e630997c4d6b10d58c3d9a7f4a5cae045130a
Christian Eckardt <eckardt.christian@web.de>
2021-05-13 14:43:18 +0200

    Revert "fixing cors"
    This reverts commit d92ea79cbf2a70452b697dd2dab9c4a15c8d26bc.

--- spaceshooter-backend.js
+++ spaceshooter-backend.js
@@ -40,7 +40,7 @@ app.get('/', function (req, res) {
 
 
 app.post('/post', function(request,response){
-  console.log('Request was: ' + request)
+  console.log(request)
   con.query('INSERT INTO highscores (Player,Score) VALUES (?, ?)',[request.body.Player,request.body.Score], function (err) {
     if(err) throw err;
     else {response.send("Received request")};

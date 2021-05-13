baf4d87388c59d6e6932e6e6cdc5d64531270062
EckeEcke <eckardt.christian@web.de>
2021-05-13 14:00:07 +0200

    fixing cors

--- spaceshooter-backend.js
+++ spaceshooter-backend.js
@@ -44,5 +44,6 @@ app.post('/post', function(request,response){
   con.query('INSERT INTO highscores (Player,Score) VALUES (?, ?)',[request.body.Player,request.body.Score], function (err) {
     if(err) throw err;
     else {response.send("Received request")};
-  })
+  });
+  con.end()
 })

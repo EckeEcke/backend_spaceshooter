06fdc724068fb323bd1581a923ce7d6b9e710cf0
Christian Eckardt <eckardt.christian@web.de>
2024-08-31 11:05:23 +0200

    switch to mongodb

--- spaceshooter-backend.js
+++ spaceshooter-backend.js
@@ -1,39 +1,4 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
-const port = process.env.PORT || 3000;
-const http = require('http');
-const mysql = require('mysql');
-const con = mysql.createConnection({
-  host: "sql11.freesqldatabase.com",
-  database: "sql11700098",
-  user: "sql11700098",
-  password: "XHDzp2mZny",
-  port: 3306
-});
 
-const sql = `SELECT * FROM highscores ORDER BY Score DESC`;
-
-
-con.connect(function(err) {
-  if(err) throw err;
-  console.log("Connected!");
-  con.query(sql, function (err, result) {
-     if (err) throw err;
-     console.log("Result: " + result);
-  });
-});
-
-app.use(cors());
-app.use(express.json());
-
-app.get('/', function (req, res) {
-  con.query(sql, function(err,result){
-    if (err) throw err;
-    else {
-      res.send(result);
-    }})
-}).listen(port);
 
 
 app.post('/post', function(request,response){
@@ -47,5 +12,73 @@ app.post('/post', function(request,response){
       if(err) throw err;
       else {response.send("Received request")};
   })
+  }
+})
+
+
+
+
+
+
+
+const express = require('express')
+const { MongoClient } = require("mongodb")
+const cors = require('cors')
+
+require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(cors())
+app.use(express.json())
+
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server Started at ${port}`)
+})
+
+let database
+const uri = `mongodb+srv://ceckardt254:${process.env.DATABASE_PASSWORD}@cluster0.sen83.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const run = async () => {
+  try {
+    database = client.db('js-shooter')
+    const scores = database.collection('highscores')
+  }
+  catch {
+    throw error
+  }
+}
+
+run().catch(console.dir)
+
+
+
+
+app.get('/', async (req, res) => {
+  try {
+    const highscores = await database.collection('highscores').find().toArray()
+    res.json(scores)
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
+})
+
+app.post('/post', async (request,response) => {
+  if(request.body.Player == undefined) {
+    console.log("no player defined in request body.............")
+    return
+  }
+
+  try {
+    const result = await database.collection('highscores').insertOne({
+        Player: request.body.Player,
+        Score: request.body.Score
+    })
+    response.json({ message: "Received request", result })
+  } catch (err) {
+    console.error(err);
+    response.status(500).send("Error inserting data")
   }
 })
